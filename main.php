70bc2ba54c3092d4218deed0c77f44cc275cfd96
root <root@Lonely-1015.local>
2012-07-25 00:21:09 +0400

    first commit

---
+++ main.php
@@ -0,0 +1,33 @@
+<?php
+class main_page {
+	public function init() {}
+
+	public function print_header() {}
+
+	public function print_body() {}
+
+	public function print_footer() {}
+
+	public function set_header() {}
+
+	public function set_body() {}
+	
+	public function set_footer() {}
+
+}
+
+class sql_handler {
+
+}
+
+function authorisation() {
+
+}
+
+class checker {
+
+
+}
+
+?>
+
