69ddb1d06fad0051872b41b7c800d35792e0db34
Boris <sexyborka@mail.ru>
2012-07-31 01:50:23 +0400

    sql handler and hackquest user shape

--- huser.php
+++ huser.php
@@ -1,16 +1,23 @@
 <?php
 
 class HUser {
+  private $db;
+
   public function HUser()
   {
     //new db connection init
-    //
+    
   }
 
   public function auth($user, $pass)
   {
     //stub
     return TRUE;
+  }
+
+  public function hasRole($roleNeed)
+  {
+
   }
 }
 
--- sql.php
+++ sql.php
@@ -9,4 +9,42 @@ function FilterSQL($str)
   return $str;
 }
 
+class baseCon {
+	//can be only psql now;
+  private $DB_type;
+  private $DB_name;
+  private $DB_username;
+  private $DB_password;
+  //now not used
+  private $DB_host;
+  private $DB_port;
+
+  public function baseCon($type)
+  {
+    D("new baseCon initialisation...<br>\n");
+    if ($type != "postgres") {
+      echo "<h2>UNKNOWN DB TYPE</h2><br>\n";
+      die();
+    }
+    $this->DB_type = $type;
+  }
+
+  public function connect($db, $username, $pass, $host, $port)
+  {
+    
+  }
+
+  public function execute($sql, $start_row = 1, $num_rows = -1)
+  {
+
+  }
+
+  public function error_msg()
+  {
+
+  }
+
+}
+
+
 ?>
