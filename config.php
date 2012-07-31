bf21e33396d6befb15ac77d818239e8a66353d5f
Boris <sexyborka@mail.ru>
2012-07-31 08:37:09 +0400

    huser class changes

--- config.php
+++ config.php
@@ -5,10 +5,12 @@ $IS_DEBUG = 1;
 
 /* database values(used by auth.php and sql.php */
 $DB_name = "hackquest";
-$DB_type = "";
+// type can be only postgres now
+$DB_type = "postgres";
 $DB_user = "ruyk";
 $DB_password = "";
-
+$DB_host = "127.0.0.1";
+$DB_port = "5432";
 
 
 ?>
--- huser.php
+++ huser.php
@@ -5,8 +5,15 @@ class HUser {
 
   public function HUser()
   {
+    global  $DB_name, $DB_type, $DB_user,
+    $DB_password, $DB_host, $DB_port;
+
     //new db connection init
+    $db = new baseCon($DB_type);
+    //FIXME: complete me
+    $db->connect($DB_name, $DB_user, $DB_password, $DB_host, $DB_port);
     
+    $this->db = $db;
   }
 
   public function auth($user, $pass)
